355227865bc928202132d9c52d71c38530d42989
vishalparjapat10 <vishalparjapat12527@gmail.com>
2022-03-19 17:14:29 +0530

    organize command implemented

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ function help(){
     These are some myCLI commands used in various situations:-
         1. node main.js tree <path>
         2. node main.js organize <path>
-        3. node main.js help <path>
+        3. node main.js help 
     `);
     
 
---
+++ commands/organize.js
@@ -0,0 +1,86 @@
+const fs = require("fs");
+const path = require("path");
+
+let types = {
+    media: ["mp4", "mkv", "mp3"],
+    archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', "xz"],
+    documents: ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex'],
+    app: ['exe', 'dmg', 'pkg', "deb"],
+    images: ['png','jpg','jpeg']
+}
+
+function organize(srcPath){
+    // 1. to check if srcPath is present
+    if(srcPath == undefined){
+        srcPath = process.cwd();//The process.cwd() method returns the current working directory of the Node.js process.
+        console.log(srcPath);
+    }
+
+    // 2. to create a directory by the name of organized_files
+    // let organizedFiles = srcPath + "/" + "organized_files";
+    // OR
+    let organizedFiles = path.join(srcPath,"organized_files");// this will join organized-files folder to the srcPath and will return the path
+    console.log(organizedFiles);
+    // checking if there is already folder available or not, if it is not then only we can make folder otherwise it will give error if we again make folder with the same name
+    if(!fs.existsSync(organizedFiles)){
+        fs.mkdirSync(organizedFiles);// this will create the folder at newly created/joined path
+    }
+    else{
+        console.log("Folder already exists");
+    }
+    // 3. scan the enire srcPath(downloads folder in our case) 
+
+    let allFiles = fs.readdirSync(srcPath);// reads the content of the directory (inside downloads folder in our case) i.e reads the names of files present inside downloads folder
+
+    console.log(allFiles);
+    // 4. traverse over all the files and classify them on the basis of their extension(.pdf,.mp3 etc.)
+
+    for(let i = 0;i < allFiles.length;i++){
+
+        let fullPathOfFile = path.join(srcPath,allFiles[i]);
+        // console.log(fullPathOfFile);
+        // lstatSync gives the info regarding the link provided 
+        
+        // 1. Check if it is a file or folder
+        let isFile = fs.lstatSync(fullPathOfFile).isFile();
+        if(isFile){
+            // 1.1 get ext name
+            let ext = path.extname(allFiles[i]).split(".")[1];
+            console.log(ext);
+            // 1.2 get folder name from extension
+            let folderName = getFolderName(ext);
+            console.log(folderName);
+            // 1.3 copy from src folder(srcPath) and paste in dest folder (folder_name e.g document,media etc.)
+            // srcPath - the path from where the file to be copied
+            // fullPathOfFile - the full path file filw which is to be copied
+            // folderName - destination folder where file is to be paste
+            copyFileToDest(srcPath,fullPathOfFile,folderName);
+        }
+            
+    }
+}
+
+function getFolderName(ext){
+    for(let key in types){
+        for(let i = 0;i < types[key].length ;i++){
+            if(types[key][i] == ext){
+                return key;
+            }
+        }
+    }
+}
+
+function copyFileToDest(srcPath,fullPathOfFile,folderName){
+    // 1. first we will have to make folderName path
+    let destFolderPath = path.join(srcPath,"organized_files",folderName);
+    // 2 check folder, if exists,if it does not,then make folder
+    if(!fs.existsSync(destFolderPath)){
+        fs.mkdirSync(destFolderPath);
+    }
+    // 3. copy file from src folder to dest folder
+    let fileName = path.basename(fullPathOfFile);//Returns the last portion of a path
+    let destFileName = path.join(destFolderPath,fileName);
+    fs.copyFileSync(fullPathOfFile,destFileName)
+}
+let srcPath = "C:\\Users\\Vishal\\Downloads\\PepCoding\\Course Projects\\fileOrganizer\\downloads";
+organize(srcPath);
