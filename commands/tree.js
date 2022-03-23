dc8834435307ea6645152efaf03f4194564f9c1a
vishalparjapat10 <vishalparjapat12527@gmail.com>
2022-03-23 11:47:19 +0530

    refracting of code done

--- commands/tree.js
+++ commands/tree.js
@@ -14,16 +14,21 @@ function treeFn(dirPath) {
 }
 
 function treeHelper(targetPath, indent) {
+    // checking if it is file at thr targetPath
     let isFile = fs.lstatSync(targetPath).isFile();
 
+     // if it is a file then printing it
     if (isFile == true) {
         let fileName = path.basename(targetPath);
         console.log(indent + "├── " + fileName);
         return;
     }
+
+    // if it is not a file then it will be a directory/folder
     let dirName = path.basename(targetPath);
     console.log(indent + "└──" + dirName);
 
+    // finding all files & folder inside dirName
     let children = fs.readdirSync(targetPath);
 
     for (let i = 0; i < children.length; i++) {
