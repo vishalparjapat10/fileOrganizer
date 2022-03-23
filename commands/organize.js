7ceb25a6cceb14b79012653e9b3f920ef2dff0d1
vishalparjapat10 <vishalparjapat12527@gmail.com>
2022-03-23 11:44:14 +0530

    tree command implemented

--- commands/organize.js
+++ commands/organize.js
@@ -13,14 +13,13 @@ function organize(srcPath){
     // 1. to check if srcPath is present
     if(srcPath == undefined){
         srcPath = process.cwd();//The process.cwd() method returns the current working directory of the Node.js process.
-        console.log(srcPath);
     }
 
     // 2. to create a directory by the name of organized_files
     // let organizedFiles = srcPath + "/" + "organized_files";
     // OR
     let organizedFiles = path.join(srcPath,"organized_files");// this will join organized-files folder to the srcPath and will return the path
-    console.log(organizedFiles);
+
     // checking if there is already folder available or not, if it is not then only we can make folder otherwise it will give error if we again make folder with the same name
     if(!fs.existsSync(organizedFiles)){
         fs.mkdirSync(organizedFiles);// this will create the folder at newly created/joined path
@@ -32,13 +31,11 @@ function organize(srcPath){
 
     let allFiles = fs.readdirSync(srcPath);// reads the content of the directory (inside downloads folder in our case) i.e reads the names of files present inside downloads folder
 
-    console.log(allFiles);
     // 4. traverse over all the files and classify them on the basis of their extension(.pdf,.mp3 etc.)
 
     for(let i = 0;i < allFiles.length;i++){
 
         let fullPathOfFile = path.join(srcPath,allFiles[i]);
-        // console.log(fullPathOfFile);
         // lstatSync gives the info regarding the link provided 
         
         // 1. Check if it is a file or folder
@@ -46,13 +43,11 @@ function organize(srcPath){
         if(isFile){
             // 1.1 get ext name
             let ext = path.extname(allFiles[i]).split(".")[1];
-            console.log(ext);
             // 1.2 get folder name from extension
             let folderName = getFolderName(ext);
-            console.log(folderName);
             // 1.3 copy from src folder(srcPath) and paste in dest folder (folder_name e.g document,media etc.)
             // srcPath - the path from where the file to be copied
-            // fullPathOfFile - the full path file filw which is to be copied
+            // fullPathOfFile - the full path file file which is to be copied
             // folderName - destination folder where file is to be paste
             copyFileToDest(srcPath,fullPathOfFile,folderName);
         }
@@ -68,6 +63,7 @@ function getFolderName(ext){
             }
         }
     }
+    return "miscellaneous";// in case there is a special or undefined extension which is not mentioned in "types"
 }
 
 function copyFileToDest(srcPath,fullPathOfFile,folderName){
@@ -82,5 +78,9 @@ function copyFileToDest(srcPath,fullPathOfFile,folderName){
     let destFileName = path.join(destFolderPath,fileName);
     fs.copyFileSync(fullPathOfFile,destFileName)
 }
-let srcPath = "C:\\Users\\Vishal\\Downloads\\PepCoding\\Course Projects\\fileOrganizer\\downloads";
-organize(srcPath);
+// let srcPath = "C:\\Users\\Vishal\\Downloads\\PepCoding\\Course Projects\\fileOrganizer\\downloads";
+
+
+module.exports = {
+    organize:organize
+}
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let helpFunc = require('./commands/help');
-
+let orgFun = require("./commands/organize");
+let treeFunc = require("./commands/tree")
 let inputArr = process.argv.slice(2);
 let command = inputArr[0];
 let path = inputArr[1];
@@ -7,9 +8,11 @@ let path = inputArr[1];
 switch(command){
     case "tree":
         // call tree function
+        treeFunc.tree(path);
         break;
     case "organize": 
         // call organize function
+        orgFun.organize(path);
         break;
     case "help":
         // call help function
